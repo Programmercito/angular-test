58c7625ad31aafa8086b10126c3a51e1d1ba5916
usern4445346 <user36536@mail.com>
2023-12-07 23:27:07 -0400

    test unitarios de todos menos de post

--- src/app/shared/login/login.component.spec.ts
+++ src/app/shared/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { AppModule } from 'src/app/app.module';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +9,8 @@ describe('LoginComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [LoginComponent],
+      imports:[AppModule]
     });
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
