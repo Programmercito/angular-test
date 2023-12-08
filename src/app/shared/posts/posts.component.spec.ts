d1ab80e33fa472c04dab6c5e265bce2746c4dac3
usern4445346 <user36536@mail.com>
2023-12-08 00:54:27 -0400

    pruebas unitarias de post

--- src/app/shared/posts/posts.component.spec.ts
+++ src/app/shared/posts/posts.component.spec.ts
@@ -1,21 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PostsComponent } from './posts.component';
+import { PostsService } from 'src/app/core/posts.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { AppModule } from 'src/app/app.module';
+import { By } from '@angular/platform-browser';
 
 describe('PostsComponent', () => {
   let component: PostsComponent;
-  let fixture: ComponentFixture<PostsComponent>;
+  let postsService: PostsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PostsComponent]
+      declarations: [PostsComponent],
+      providers: [PostsService,
+        ConfirmationService,
+        MessageService],
+      imports: [HttpClientTestingModule,
+        AppModule]
     });
-    fixture = TestBed.createComponent(PostsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('create modal add', async () => {
+    const fixture = TestBed.createComponent(PostsComponent);
+    fixture.detectChanges();
+    const element = fixture.nativeElement.querySelector('#addbutton');
+    element.click();
+    fixture.detectChanges();
+    await fixture.whenStable();
+    const html = fixture.nativeElement.innerHTML;
+    const element2 = fixture.nativeElement.querySelector('#spam');
+    expect(element2.textContent).toContain('Post add');
+
   });
+ 
+
+  it('title of posts', () => {
+    const fixture = TestBed.createComponent(PostsComponent);
+    fixture.detectChanges();
+    const html = fixture.nativeElement.innerHTML;
+    console.log(html);
+    const element = fixture.nativeElement.querySelector('.p-card-title');
+    expect(element.textContent).toContain('Posts');
+  });
+
+
 });
